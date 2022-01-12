c05c1e3d43f3b6682ce91854a61aa5089059f94e
Tejas <tejas14all@gmail.com>
2022-01-12 20:41:02 +0530

    filter

---
+++ src/com/controller/FindAgeSerlvet.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FindAgeSerlvet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		System.out.println("FindAgeServlet::service()");
+		int byear = Integer.parseInt( request.getParameter("byear"));
+		//1988
+		//2022-1988
+		Calendar c = Calendar.getInstance(); 
+		int currentYear = c.get(Calendar.YEAR);
+		
+		int age = currentYear-byear;
+		
+		request.setAttribute("age",age);
+
+		RequestDispatcher rd = request.getRequestDispatcher("AgeResult.jsp");
+		rd.forward(request, response);
+		
+	}
+}
---
+++ src/com/filter/AgeValidationFilter.java
@@ -0,0 +1,63 @@
+package com.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+//
+public class AgeValidationFilter implements Filter {
+
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+	// service
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		boolean isError = false;
+		System.out.println("AgeValidationFilter::doFilter()");
+
+		// goback with error
+
+		String byear = request.getParameter("byear");
+		if (byear == null || byear.trim().length() == 0) {
+			isError = true;
+			request.setAttribute("error", "Please Enter Birth Year");
+		} else {
+
+			try {
+				int birthYear = Integer.parseInt(byear);
+				if (birthYear > 2022) {
+					isError = true;
+					request.setAttribute("error", "Please Enter Correct Birth Year");
+				}
+			} catch (Exception e) {
+				isError = true;
+				request.setAttribute("error", "Please Enter Valid Birth Year");
+			}
+
+		}
+
+		if (isError == true) {
+			// bo back
+			RequestDispatcher rd = request.getRequestDispatcher("AgeCalculator.jsp");
+			rd.forward(request, response);
+		} else {
+
+			// go ahead
+			chain.doFilter(request, response);// go ahead
+		}
+	}
+
+	public void destroy() {
+
+	}
+
+}
